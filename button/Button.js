2849653a895e0c4203559a96910e36d67d1e5e8d
JoseJR2202 <65959467+JoseJR2202@users.noreply.github.com>
2021-06-09 02:52:31 -0400

    Botones
    Version pre final

--- button/Button.js
+++ button/Button.js
@@ -2,12 +2,35 @@ import VisualComponent from "../VisualComponent.js";
 
 class Button extends VisualComponent {
 
-    constructor() {
+    constructor(tipo){
         super();
         this.button = document.createElement('button');
-        this.button.innerHTML = 'creo que soy un boton';
         this.wrapper.appendChild(this.button);
+        this.button.className =verificar(tipo)? tipo:'ButtonBasic';// Basic estara por defecto
     }
+
+    setText(texto){
+        this.button.innerText=texto;
+    }
+
+    setOnclikEvent(event){
+        this.button.addEventListener("click",event);
+    }
+
+    setMouseOver(event){
+        this.button.addEventListener("mouseover",event);
+    }
+
+    setDisabled(valor){
+        this.button.setAttribute("disabled",valor)
+    }
+
+}
+
+//verificar si es uno de los tipos exitentes
+function verificar(params) {
+    if(params==="ButtonBig"||params==="ButtonBasic"||params==="ButtonMargen")
+        return true;
 }
 
 export default Button;
--- prueba.js
+++ prueba.js
@@ -1,10 +1,35 @@
 import Button from "./button/button.js";
 
-var button = new Button();
+var button = new Button('ButtonMargen');
+var button2 = new Button('ButtonBig');
+var button3= new Button();
 
 button.draw();
-button.setPosition(150, 100);
+button.setPosition(0, 100);
 console.log(button.position);
 button.setDimension(200, 100);
 console.log(button.dimension);
+button.setText("boton de prueba");
+button.setMouseOver(()=>{
+    console.log("hola");
+})
+button.setDisabled("true");
 button.appendBody();
+
+button2.draw();
+button2.setPosition(150, 0);
+console.log(button.position);
+button2.setDimension(200, 100);
+console.log(button.dimension);
+button2.setText("boton de prueba");
+//button2.setDisabled("true");
+button2.appendBody();
+
+button3.draw();
+button3.setPosition(300, -100);
+console.log(button.position);
+button3.setDimension(200, 100);
+console.log(button.dimension);
+button3.setText("boton de prueba");
+button2.setDisabled("true");
+button3.appendBody();
