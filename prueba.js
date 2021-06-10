2a34e52446e37000bf81694a31d3d7fa5131c29a
JoseJR2202 <65959467+JoseJR2202@users.noreply.github.com>
2021-06-09 20:49:58 -0400

    mini bug arreglado

--- prueba.js
+++ prueba.js
@@ -6,7 +6,7 @@ var button2 = new Button('ButtonBig');
 var button3 = new Button();
 
 button.draw();
-button.setPosition(0, 100);
+button.setPosition(0, 0);
 console.log(button.position);
 button.setDimension(140, 20);
 console.log(button.dimension);
@@ -17,29 +17,28 @@ button.setMouseOver(() => {
 button.setDisabled(false);
 
 button2.draw();
-button2.setPosition(170, 80);
+button2.setPosition(170, -20);
 console.log(button.position);
-button.setDimension(140, 20);
+button2.setDimension(140, 20);
 console.log(button.dimension);
 button2.setText("boton de prueba");
 //button2.setDisabled(true);
 
 button3.draw();
-button3.setPosition(340, 60);
+button3.setPosition(340, -40);
 console.log(button.position);
-button.setDimension(140, 20);
+button3.setDimension(140, 20);
 console.log(button.dimension);
 button3.setText("boton de prueba");
 //button3.setDisabled(true);
-
 // Card
 
-// var card = new Card();
-// card.draw();
-// card.setSubtitle('DEE');
-// card.setTitle('DAA');
-// card.setContent('DIII');
-// card.setImg('./resources/xd.png');
-// card.setImgAlt('No tenia más fotos en mi PC y se me fue el inter');
-// card.setFooter("Footer chiquito");
-// card.setImgFloat('left');
+var card = new Card();
+card.draw();
+card.setSubtitle('DEE');
+card.setTitle('DAA');
+card.setContent('DIII');
+card.setImg('./resources/xd.png');
+card.setImgAlt('No tenia más fotos en mi PC y se me fue el inter');
+card.setFooter("Footer chiquito");
+card.setImgFloat('left');
