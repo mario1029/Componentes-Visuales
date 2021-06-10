697accacfa00b60678410288f418686a9d70cd2a
JoseJR2202 <65959467+JoseJR2202@users.noreply.github.com>
2021-06-09 20:31:48 -0400

    button Version final

--- prueba.js
+++ prueba.js
@@ -8,38 +8,38 @@ var button3 = new Button();
 button.draw();
 button.setPosition(0, 100);
 console.log(button.position);
-button.setDimension(200, 100);
+button.setDimension(140, 20);
 console.log(button.dimension);
 button.setText("boton de prueba");
 button.setMouseOver(() => {
     console.log("hola");
 })
-button.setDisabled("true");
+button.setDisabled(false);
 
 button2.draw();
-button2.setPosition(150, 0);
+button2.setPosition(170, 80);
 console.log(button.position);
-button2.setDimension(200, 100);
+button.setDimension(140, 20);
 console.log(button.dimension);
 button2.setText("boton de prueba");
-//button2.setDisabled("true");
+//button2.setDisabled(true);
 
 button3.draw();
-button3.setPosition(300, -100);
+button3.setPosition(340, 60);
 console.log(button.position);
-button3.setDimension(200, 100);
+button.setDimension(140, 20);
 console.log(button.dimension);
 button3.setText("boton de prueba");
-button2.setDisabled("true");
+//button3.setDisabled(true);
 
 // Card
 
-var card = new Card();
-card.draw();
-card.setSubtitle('DEE');
-card.setTitle('DAA');
-card.setContent('DIII');
-card.setImg('./resources/xd.png');
-card.setImgAlt('No tenia más fotos en mi PC y se me fue el inter');
-card.setFooter("Footer chiquito");
-card.setImgFloat('left');
+// var card = new Card();
+// card.draw();
+// card.setSubtitle('DEE');
+// card.setTitle('DAA');
+// card.setContent('DIII');
+// card.setImg('./resources/xd.png');
+// card.setImgAlt('No tenia más fotos en mi PC y se me fue el inter');
+// card.setFooter("Footer chiquito");
+// card.setImgFloat('left');
